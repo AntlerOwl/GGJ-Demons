349857bd907eb7a99562b13cb46fbb5bc256ec67
TordTrøen <assessedgrub@gmail.com>
2016-01-30 19:42:55 +0100

    Added removing from the summon bench

--- DemonsPleaseGGJ2016/Assets/Scripts/UI/UIDragManager.cs
+++ DemonsPleaseGGJ2016/Assets/Scripts/UI/UIDragManager.cs
@@ -7,9 +7,12 @@ public class UIDragManager : MonoBehaviour
     public static UIDragManager instance;
     public Image dragImage;
     public Ingredient curIngredient;
+    private int fromSummonSlot = -1;
+    private SummoningManager summoningManager;
 
     void Awake()
     {
+        summoningManager = GameManager.instance.SummoningManager;
         if (instance)
         {
             Debug.Log("More than one instance of UIDragManager exists!", this);
@@ -26,23 +29,34 @@ public class UIDragManager : MonoBehaviour
         {
             if (Input.GetMouseButtonUp(0))
             {
+                if (fromSummonSlot > -1)
+                {
+                    summoningManager.RemoveIngredient(fromSummonSlot);
+                }
                 EndDrag();
             }
         }
     }
 
-    public void BeginDrag(Ingredient ingredient)
+    public void BeginDrag(Ingredient ingredient, int fromSummonSlot = -1)
     {
         dragImage.gameObject.SetActive(true);
         dragImage.sprite = ingredient.icon;
         curIngredient = ingredient;
+        this.fromSummonSlot = fromSummonSlot;
     }
 
     public void EndDrag()
     {
-        print("End drag");
         dragImage.gameObject.SetActive(false);
+        StartCoroutine(ResetIngredientAndSlot());
+    }
+
+    IEnumerator ResetIngredientAndSlot()
+    {
+        yield return new WaitForEndOfFrame();
         curIngredient = null;
+        fromSummonSlot = -1;
     }
 
     public void SetPosition()
--- DemonsPleaseGGJ2016/Assets/Scripts/UI/UISummonItem.cs
+++ DemonsPleaseGGJ2016/Assets/Scripts/UI/UISummonItem.cs
@@ -1,26 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UISummonItem : UIEndDrag
+public class UISummonItem : UIEndDrag, IDragHandler, IBeginDragHandler
 {
     public Ingredient ingredient;
     public Image iconImage;
     private SummoningManager summoningManager;
-    private int summonSlotId;
+    [HideInInspector]public int summonSlotId;
 
     void Awake()
     {
         summoningManager = GameManager.instance.SummoningManager;
-        summonSlotId = transform.GetSiblingIndex();
     }
 
 	protected override void OnEndDrag()
     {
         ingredient = UIDragManager.instance.curIngredient;
         iconImage.sprite = ingredient.icon;
-        summoningManager.AddIngredient(ingredient, summonSlotId);
+        summoningManager. AddIngredient(ingredient, summonSlotId);
 
         UIDragManager.instance.EndDrag();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        UIDragManager.instance.SetPosition();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // report to manager that we are dragging from summons
+        UIDragManager.instance.BeginDrag(ingredient, summonSlotId);
     }
 }
